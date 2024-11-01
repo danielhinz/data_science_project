2046e2a45b4ad7306e3f7bdcee1cc1822caee46d
Daniel Hinz <danielhinz@posteo.de>
2024-11-01 10:49:00 +0100

    changes

--- scraper.py
+++ scraper.py
@@ -1,21 +1,17 @@
 import pandas as pd
 import requests
 import re
+from datetime import datetime, timedelta
+
 
 url_dict = {'Wilhelmstraße':'https://www.my-stuwe.de//wp-json/mealplans/v1/canteens/715?lang=de&v=1729619513869',
             'Prinz Karl':'https://www.my-stuwe.de//wp-json/mealplans/v1/canteens/623?lang=de&v=1729614874306',
             'Morgenstelle':'https://www.my-stuwe.de//wp-json/mealplans/v1/canteens/724?lang=de&v=1729621051545'}
 
 
-
-
-
 def run_scraper(option, date, url=url_dict):
 
-    
-
     url = url_dict[option]
-
 
     # Make the GET request
     response = requests.get(url)
@@ -33,13 +29,17 @@ def run_scraper(option, date, url=url_dict):
 
     # Convert the list of dictionaries into a new DataFrame
     menus_df = pd.DataFrame(menus_list)
+    #menus_df.to_excel("test.xlsx")
 
     menus_df.drop("photo", axis=1, inplace = True)
 
     return menus_df[menus_df["menuDate"] == date]
-    #return menus_df[~menus_df["menu"].duplicated(keep=False)]
 
 
-#print(run_scraper("Wilhelmstraße"))
+def get_dates(num_days):
+    today = datetime.today()
+    dates = [(today + timedelta(days=i)).strftime("%Y-%m-%d") for i in range(num_days)]
+    return dates
+
 
 
--- web_app.py
+++ web_app.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template,request, jsonify
-from scraper import run_scraper
+from scraper import run_scraper, get_dates
 
 app = Flask(__name__)
-
-
 
 @app.route('/')
 def index():
     # You can pass dynamic options to the dropdown if needed
     mensa_options = ["Wilhelmstraße", "Morgenstelle", "Prinz Karl"]
-    date_options = ["2024-10-24","2024-10-25","2024-10-26"]
-
+    date_options = get_dates(4)
 
     return render_template('index.html', options=mensa_options, dates=date_options)
 
@@ -18,24 +15,28 @@ def index():
 def display_res():
 
     # get option result
-    selected_option = request.form.get('dropdown')
+    selected_mensa = request.form.get('dropdown')
     selected_date = request.form.get("date")
 
     # Ensure both dropdowns are selected
-    if not selected_option or not selected_date:
-        return "Please select both an option and a date!", 400
+    if not selected_mensa or not selected_date:
+        return "Please select both a mensa and a date!", 400
 
     # get scraper results (dataframe)
-    scraper_res = run_scraper(selected_option,selected_date)
+    scraper_res = run_scraper(selected_mensa,selected_date)
+
+    # daily dish
+    table_dish = scraper_res[scraper_res["menuLine"] == "Angebot des Tages"][["menuLine","menu","studentPrice"]]
+    table_dish_vegan = scraper_res[scraper_res["menuLine"] == "Angebot d. Tages vegan"][["menuLine","menu","studentPrice"]]
+    table_dish_veget = scraper_res[scraper_res["menuLine"] == "Angebot d. Tages veget."][["menuLine","menu","studentPrice"]]
 
     # Convert the dataframe to HTML
-    df_html = scraper_res.to_html(classes='dataframe', header="true", index=False)
-
+    #df_html = scraper_res.to_html(classes='dataframe', header="true", index=False)
+    table_dish = table_dish.to_dict(orient="records")
+    table_dish_vegan = table_dish_vegan.to_dict(orient="records")
 
     # Render the template and pass the dataframe HTML to it
-   # return render_template('result.html', selected_option=selected_option, table=df_html)
-    return render_template('result.html', table=df_html, selected_option=selected_option, selected_date=selected_date)
-
+    return render_template('result.html', table_dish=table_dish,table_dish_vegan=table_dish_vegan,table_dish_veget=table_dish_veget,selected_option=selected_mensa, selected_date=selected_date)
 
 if __name__ == '__main__':
     app.run(debug=True)
